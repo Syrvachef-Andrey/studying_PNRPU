ff9bf3f01a2a064c7994d8897761e1283c15d04c
Syrvachev_A_I <an.sirvacheff@yandex.ru>
2024-11-01 10:35:24 +0500

    feat: add lesson with identification clock on image with opencv cascade

--- opencv_lessons/cv_neiro.py
+++ opencv_lessons/cv_neiro.py
@@ -1,13 +1,13 @@
 import cv2
 
-number_cascade = cv2.CascadeClassifier('cascades/haarcascade_russian_plate_number.xml')
-img = cv2.imread('images/number_2.jpg')
+clock_cascade = cv2.CascadeClassifier('cascades/haarcascade_wallclock.xml')
+img = cv2.imread('images/clock1.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-numbers = number_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=30)
-for x, y, w, h in numbers:
+clock = clock_cascade.detectMultiScale(gray, scaleFactor=1.001, minNeighbors=30)
+for x, y, w, h in clock:
     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-cv2.imshow('Number detect', img)
+cv2.imshow('Clock detect', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
--- opencv_lessons/konspect_cv.py
+++ opencv_lessons/konspect_cv.py
@@ -101,3 +101,19 @@
 # cv2.imshow('Number detect', img)
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
+
+# Обнаружение часов на картинке с помошью каскадов данных opencv
+
+# import cv2
+#
+# clock_cascade = cv2.CascadeClassifier('cascades/haarcascade_wallclock.xml')
+# img = cv2.imread('images/clock1.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#
+# clock = clock_cascade.detectMultiScale(gray, scaleFactor=1.001, minNeighbors=30)
+# for x, y, w, h in clock:
+#     cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+#
+# cv2.imshow('Clock detect', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
