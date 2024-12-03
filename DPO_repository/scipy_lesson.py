4e65dbd4afcf23e2ea1d37ddc46287823d89285e
Andrey <an.sirvacheff@yandex.ru>
2024-12-03 22:11:14 +0500

    hotfix

--- DPO_repository/scipy_lesson.py
+++ DPO_repository/scipy_lesson.py
@@ -58,3 +58,38 @@
 
 # Задача 3
 
+# import numpy as np
+# import matplotlib.pyplot as plt
+#
+# # Определение функции для проверки принадлежности к множеству Мандельброта
+# def mandelbrot(c, max_iter):
+#     z = 0
+#     for n in range(max_iter):
+#         if abs(z) > 2:
+#             return n
+#         z = z*z + c
+#     return max_iter
+#
+# # Параметры
+# xmin, xmax, xn = -2.0, 1.0, 600  # Диапазон и количество точек по оси x
+# ymin, ymax, yn = -1.5, 1.5, 600  # Диапазон и количество точек по оси y
+# max_iter = 100  # Максимальное количество итераций
+#
+# # Создание сетки значений c = x + 1j*y
+# x = np.linspace(xmin, xmax, xn)
+# y = np.linspace(ymin, ymax, yn)
+# c = x[:,np.newaxis] + 1j*y[np.newaxis,:]
+#
+# # Выполнение итераций и формирование маски
+# mask = np.zeros_like(c, dtype=bool)
+# for i in range(xn):
+#     for j in range(yn):
+#         mask[i, j] = mandelbrot(c[i, j], max_iter) == max_iter
+#
+# # Создание изображения
+# plt.figure(figsize=(10, 10))
+# plt.imshow(mask.T, extent=[xmin, xmax, ymin, ymax], cmap='binary')
+# plt.title('Множество Мандельброта')
+# plt.xlabel('Re(c)')
+# plt.ylabel('Im(c)')
+# plt.show()
