4ba30bf330b0990dd665d53844f40beaf4478d3c
Andrey <an.sirvacheff@yandex.ru>
2024-12-05 21:55:24 +0500

    hotfix in 9_number.py

--- math_labs/9_number.py
+++ math_labs/9_number.py
@@ -1,35 +1,43 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from scipy.fft import fft, ifft
+import matplotlib.pyplot as plt
 
-print("Введите диапазон определений по условию (0, 4)")
+# Определяем функцию, которую будем разлагать в ряд Фурье
+def f(x):
+    return 4 - x
 
-while True:
-    print("Введите количество элементов в ряде: ", end='')
-    n = int(input())
-    if type(n) != int or n <= 4:
-        print("Неправильно введено значение количества элементов")
-        print("Введите заново")
-    else:
-        break
+# Параметры разложения
+L = 4  # Период функции (в данном случае, функция определена на интервале [-L, L])
+N = 100  # Количество членов ряда Фурье
 
-x_arr = np.array([])
-y_arr = np.array([])
+# Создаем массив точек для вычисления интегралов
+x = np.linspace(0, 4, 1000)
 
-step = (4 - 0) / n
-x = 0
+# Вычисление коэффициентов Фурье
+a0 = (1 / L) * np.trapz(f(x), x)
+an = lambda n: (1 / L) * np.trapz(f(x) * np.cos(n * np.pi * x / L), x)
+bn = lambda n: (1 / L) * np.trapz(f(x) * np.sin(n * np.pi * x / L), x)
 
-while x < 4:
-    y = 4 - x
-    x_arr = np.append(x_arr, x)
-    y_arr = np.append(y_arr, y)
-    x += step
+# Вычисление ряда Фурье
+def fourier_series(x, N):
+    result = a0 / 2
+    for n in range(1, N + 1):
+        result += an(n) * np.cos(n * np.pi * x / L) + bn(n) * np.sin(n * np.pi * x / L)
+    return result
 
-y_arr_ryad = fft(x_arr)
-print(y_arr_ryad)
-plt.plot(x_arr, y_arr, x_arr, y_arr_ryad)
+# Создаем массив точек для построения графика
+x_plot = np.linspace(0, 4, 1000)
+
+# Вычисляем значения функции и ее аппроксимации рядом Фурье
+y_exact = f(x_plot)
+y_approx = fourier_series(x_plot, N)
+
+# Построение графика
+plt.figure(figsize=(10, 6))
+plt.plot(x_plot, y_exact, label='Exact function: $4 - x$', linewidth=2)
+plt.plot(x_plot, y_approx, label=f'Fourier series (N={N})', linestyle='--', linewidth=2)
+plt.title('Fourier Series Approximation of $4 - x$')
 plt.xlabel('x')
 plt.ylabel('y')
-plt.title('График функции y = 4 - x')
+plt.legend()
 plt.grid(True)
 plt.show()
