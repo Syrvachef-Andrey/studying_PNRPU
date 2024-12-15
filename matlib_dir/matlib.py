7547164b7977d17a041c4f5105656594b5415a06
Andrey <an.sirvacheff@yandex.ru>
2024-12-15 15:44:35 +0500

    feat: new lesson on matlib

--- matlib_dir/matlib.py
+++ matlib_dir/matlib.py
@@ -61,18 +61,40 @@
 
 # Создание графика с подписью осей и заголовком
 
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# plt.figure(num="My plot", figsize=[7.0, 5.0], dpi=100)
+#
+# x = np.linspace(0, np.pi * 2, 100)
+#
+# y = np.sin(x)
+#
+# plt.plot(x, y, label="y = sin(x)")
+# plt.title(label="Синусоида", loc="center")
+# plt.grid()
+# plt.xlabel(xlabel="Ось X", loc="center", color='red')
+# plt.ylabel(ylabel="Ось Y", loc="center", color='red')
+# plt.show()
+
+# Создание графика синуса с ограничениями с отобажениями
+
 import matplotlib.pyplot as plt
 import numpy as np
 
-plt.figure(num="My plot", figsize=[7.0, 5.0], dpi=100)
+plt.figure(num="my plot", figsize=[6.0, 5.0], dpi=100)
 
 x = np.linspace(0, np.pi * 2, 100)
 
 y = np.sin(x)
 
 plt.plot(x, y, label="y = sin(x)")
-plt.title(label="Синусоида", loc="center")
+
+plt.title(label="y = sin(x)", loc="center")
 plt.grid()
-plt.xlabel(xlabel="Ось X", loc="center", color='red')
-plt.ylabel(ylabel="Ось Y", loc="center", color='red')
+plt.xlabel(xlabel="Ось X", loc="center")
+plt.ylabel(ylabel="Ось Y", loc="center")
+plt.ylim(top=0.75)
+plt.xlim(left=0, right=6)
+
 plt.show()
