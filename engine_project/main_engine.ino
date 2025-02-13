6cb06f53905d54823f99e0b112610ed0f8834003
andrey <an.sirvacheff@yandex.ru>
2025-02-13 18:24:27 +0500

    feat: final version of test main_engine.ino

--- engine_project/main_engine.ino
+++ engine_project/main_engine.ino
@@ -1,3 +1,6 @@
+#include "OneWire.h"
+#include "MAX31850.h"
+
 #define svecha 2
 #define RPWM_S 3
 #define LPWM_S 4
@@ -8,14 +11,20 @@
 #define REN_N 9
 #define LEN_N 10
 #define button 11
+#define ONE_WIRE_BUS 12
+#define hallPin 13
 
-int value = 0; 
-int lastButtonState = LOW; 
-int currentButtonState;   
-bool isPressed = false; 
+int value = 0;
+int lastButtonState = LOW;
+int currentButtonState;
+bool isPressed = false;
+unsigned int hall = 0;
+
+OneWire oneWire(ONE_WIRE_BUS);
+MAX31850 sensor(&oneWire);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
 
   pinModde(svecha, 1);
   pinMode(RPWM_S, 1);
@@ -23,28 +32,56 @@ void setup() {
   pinMode(LEN_S, 1);
   pinMode(REN_S, 1);
   digitalWrite(REN_S, HIGH);
-  digitalWrite(LEN_S, HIGH); 
+  digitalWrite(LEN_S, HIGH);
   pinMode(RPWM_N, 1);
   pinMode(LPWM_N, 1);
   pinMode(LEN_N, 1);
   pinMode(REN_N, 1);
   digitalWrite(REN_N, HIGH);
-  digitalWrite(LEN_N, HIGH);  
+  digitalWrite(LEN_N, HIGH);
   pinMode(button, 0);
+  pinMode(hallPin, 0);
+
+  sensor.begin();
+  sensor.requestTemperatures();
 }
 
 void loop() {
   currentButtonState = digitalRead(button);
-
-  if (currentButtonState == HIGH && lastButtonState == LOW && !isPressed) {
-    isPressed = true; 
-    value = (value == 0) ? 1 : 0; 
-    Serial.println(value); 
+  if (digitalRead(hallPin) > 0){
+    hall++;
+    }
+  if (currentButtonState == 1 and lastButtonState == 0 and !isPressed) {
+    isPressed = 1;
+    value = (value == 0) ? 1 : 0;
+    Serial.println(value);
   }
 
-  if (currentButtonState == LOW) {
-    isPressed = false;
+  if (currentButtonState == 0) {
+    isPressed = 0;
   }
-
     lastButtonState = currentButtonState;
+  if (currentButtonState == 1 and sensor.isCoversionComplete()){
+    if (sensor.requestTemperatures() < 150 and (hall / 1000) < 10000){
+    analogWrite(LPWM_S, 230);
+    analogWrite(RPWM_S, 0);
+    analogWrite(LPWM_N, 230);
+    analogWrite(RPWM_N, 0);
+    digitalWrite(svecha, 1);
+    }
+    else if(sensor.requestTemperatures() > 150 and (hall / 1000) > 10000){
+    analogWrite(LPWM_S, 0);
+    analogWrite(RPWM_S, 0);
+    analogWrite(LPWM_N, 230);
+    analogWrite(RPWM_N, 0);
+    digitalWrite(svecha, 1);
+    }
+    else if(sensor.requestTemperatures() > 150 and (hall / 1000) > 30000){
+    analogWrite(LPWM_S, 180);
+    analogWrite(RPWM_S, 0);
+    analogWrite(LPWM_N, 0);
+    analogWrite(RPWM_N, 0);
+    digitalWrite(svecha, 0);
+    }
+  }
 }
