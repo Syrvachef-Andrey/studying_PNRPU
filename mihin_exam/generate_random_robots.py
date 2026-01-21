c61de8be504a5f24e8eee8843ec1134ad8322664
Андрей <an.sirvacheff@yandex.ru>
2026-01-21 20:47:29 +0500

    fix: generate_random_robots.py fix output

--- mihin_exam/generate_random_robots.py
+++ mihin_exam/generate_random_robots.py
@@ -6,11 +6,11 @@ rand_numbers = []
 for i in range(50):
     rand_numbers.append(random.random())
 
-lambda_input = 10
+lambda_input = 5
 output_numbers = []
 
 for i in range(len(rand_numbers)):
     output_numbers.append(-1 / lambda_input * math.log(rand_numbers[i], math.exp(1)))
 
 for i in range(len(output_numbers)):
-    print(output_numbers[i], rand_numbers[i])
+    print('Вычисленное число по формуле:', round(output_numbers[i], 2), '                Случайное число из нормального распределения, нужно для проверки и подстановки в формулу', round(rand_numbers[i], 2))
